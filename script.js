59b39d4915c577b8d8d34f1b2b4aea20e7f3be95
Johannes Helleve <Johanshelleve@gmail.com>
2022-06-22 16:52:03 +0200

    added sticky

--- script.js
+++ script.js
@@ -1,16 +1,2 @@
-function myMove() {
-    let id = null;
-    const elem = document.getElementById("animate");   
-    let pos = 0;
-    clearInterval(id);
-    id = setInterval(frame, 5);
-    function frame() {
-      if (pos == 350) {
-        clearInterval(id);
-      } else {
-        pos++; 
-        elem.style.top = pos + "px"; 
-        elem.style.left = pos + "px"; 
-      }
-    }
-  }
+
+showMore = () => document.getElementById('more').style.visibility = "green";
