39d36019c2002906f00f61de51bb1a3cefcda2b7
Johannes Helleve <Johanshelleve@gmail.com>
2022-06-23 15:32:51 +0200

    visibility hidden

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
-showMore = () => document.getElementsByClass('more').st = "hidden";
-function test (){
-    document.getElementById('more').style.visibility = "hidden";
+showMore = (projectNumber) => {
+    const allElem = document.getElementsByClassName('more');
+    const elem = allElem.item(projectNumber);
+    const hiddenOrVisible = getComputedStyle(elem).visibility;
+    hiddenOrVisible === "hidden" ? elem.style.visibility = "visible" : elem.style.visibility = "hidden";
 }
