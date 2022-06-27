d923f317c00c6f321fea0e34e8bf2aa314a9e14e
Johannes Helleve <Johanshelleve@gmail.com>
2022-06-27 19:00:48 +0200

    added page transitions

---
+++ index.js
@@ -0,0 +1,29 @@
+window.onload = () => {
+  const anchors = document.querySelectorAll('a');
+  const transition_el = document.querySelector('.fade');
+  const transition_el2 = document.querySelector(".slide")
+
+  setTimeout(() => {
+    transition_el.classList.remove('is-active');
+  }, 500);
+
+  for (let i = 0; i < anchors.length; i++) {
+    const anchor = anchors[i];
+    console.log(anchor);
+    
+    anchor.addEventListener('click', e => {
+      e.preventDefault();
+      let target = e.target.href;
+
+      console.log(transition_el);
+
+      transition_el2.classList.add('is-active');
+
+      console.log(transition_el);
+
+      setInterval(() => {
+        window.location.href = target;
+      }, 500);
+    })
+  }
+}
--- script.js
+++
@@ -1,7 +0,0 @@
-
-showMore = (projectNumber) => {
-    const allElem = document.getElementsByClassName('more');
-    const elem = allElem.item(projectNumber);
-    const hiddenOrVisible = getComputedStyle(elem).visibility;
-    hiddenOrVisible === "hidden" ? elem.style.visibility = "visible" : elem.style.visibility = "hidden";
-}
