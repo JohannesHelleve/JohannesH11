1d98da439fe5d2b6933fc68d50c4d025c4cbabbf
Johannes Helleve <Johanshelleve@gmail.com>
2022-08-03 10:30:49 +0200

    responsive footer

--- index.js
+++ index.js
@@ -26,24 +26,3 @@ window.onload = () => {
 }
 
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-     document.getElementById("navbar").style.top = "0";
-    } else {
-      document.getElementById("navbar").style.top = "-250px";
-    }
-    prevScrollpos = currentScrollPos;
-
-}
-
-menu = () => {
-  var status =  document.getElementById('dropdown-content');
-  if(status.style.visibility === "visible"){
-    status.style.visibility = "hidden";
-  }else{
-    status.style.visibility = "visible";
-  }
-  console.log("test");
-}
